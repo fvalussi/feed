62de614dd83568b9ab919bc9d14feaff2da562cb
Edgar <edgargmc@gmail.com>
2020-06-20 02:20:12 -0300

    install mockito

---
+++ src/test/mock.test.ts
@@ -0,0 +1,10 @@
+// @ts-ignore
+
+describe('App Navigator Presenter should', () => {
+    test('load application summaries on start', async () => {
+        expect(true).toBe(true)
+    })
+})
+
+
+beforeEach(() => {})
