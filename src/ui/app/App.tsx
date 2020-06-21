8085cabe66204eaee4b106758bbc6aa7a1d0f584
Edgar <edgargmc@gmail.com>
2020-06-21 13:23:48 -0300

    remove styles

--- src/ui/app/App.tsx
+++ src/ui/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from '../logo.svg'
 
 import { Text } from '../components'
 
@@ -8,21 +7,8 @@ import './App.css'
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn hola
-        </a>
+      <div>App</div>
         <Text />
-      </header>
     </div>
   )
 }
