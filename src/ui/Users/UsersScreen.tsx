400f5d8168385ad4bd5ec4ddf39d6d2408160da5
Edgar <edgargmc@gmail.com>
2020-06-22 10:35:48 -0300

    wip create react route

---
+++ src/ui/Users/UsersScreen.tsx
@@ -0,0 +1,8 @@
+import {Text} from '@react-md/typography'
+import React, {FC} from 'react'
+
+export const UsersScreen: FC = () => (
+    <Text type="headline-4" align="center">
+        Users Screen
+    </Text>
+)
--- src/ui/app/Layout.tsx
+++ src/ui/app/Layout.tsx
@@ -2,16 +2,12 @@ import React, { ReactElement, FC } from 'react'
 import { Layout, useLayoutNavigation } from '@react-md/layout'
 import { Text } from '@react-md/typography'
 import { Route, Switch, useLocation, Link } from 'react-router-dom'
+import {UsersScreen} from '../Users/UsersScreen'
 import navItems from './navItems'
 
 const Home: FC = () => (
     <Text type="headline-4" align="center">
         Home Page!
-    </Text>
-)
-const Route1: FC = () => (
-    <Text type="headline-4" align="center">
-        Route 1
     </Text>
 )
 
@@ -26,7 +22,7 @@ export default (): ReactElement => {
         >
             <Switch>
                 <Route path="/" exact={true} component={Home} />
-                <Route path="/users" component={Route1} />
+                <Route path="/users" component={UsersScreen} />
             </Switch>
         </Layout>
     )
