2890ee64e4af344b83dc5ba07a94599a5a425a8f
Federico Valussi <fvalussi@veritran.com>
2020-09-04 11:12:03 -0300

    Post Card wip

--- src/ui/components/PostComponent.tsx
+++ src/ui/components/PostComponent.tsx
@@ -1,36 +1,64 @@
 import React from 'react'
-import {Post} from '../../core/domain/post/Post'
+import { Post } from '../../core/domain/post/Post'
 import styled from 'styled-components'
 
-const PostComponent = (props: { post: Post }) => {
-    const {Container, Image} = createLayout()
+const PostComponent = (props: {post: Post}) => {
+    const {Card, Image, Title, Author, Description, Content} = createLayout()
 
     return (
-        <Container>
+        <Card>
             <Image/>
-            <div>{props.post.title}</div>
-            <div>{props.post.author} - {props.post.date.toLocaleString()}</div>
-            <div>{props.post.content}</div>
-        </Container>
+            <Content>
+                <Title>{props.post.title}</Title>
+                <Description>{props.post.content}</Description>
+            </Content>
+            <Author>{props.post.author} - {props.post.date.toLocaleString()}</Author>
+        </Card>
     )
 }
 
 function createLayout() {
-    const Container = styled.div`
-  background-color: white;
-  margin: 10px;
-  height: 300px;
-  width: 250px;
-  flex-wrap: wrap;
-  overflow: hidden;
-  padding: 10px;
+    const Card = styled.div`
+display: flex;
+flex-direction: column;
+      background-color: white;
+      margin: 10px;
+      height: 300px;
+      width: 250px;
+      overflow: hidden;
+      padding: 10px;
 `
     const Image = styled.img`
+flex: 1;
       background-color: green;
-      height: 50%;
+      min-height: 150px;
+      max-height: 150px;
       min-width: 100%;
 `
-    return {Container, Image}
+    const Title = styled.div`
+      font-size: large;
+      color: mediumaquamarine;
+`
+    const Description = styled.div`
+      max-height: 110px;
+      //text-overflow: ellipsis;
+      //white-space: nowrap;
+      //overflow: hidden;
+`
+    const Author = styled.div`
+flex: 1;
+flex-basis: min-content;
+align-self: end;
+font-size: small;
+max-height: 20px;
+`
+
+    const Content = styled.div`
+      flex: 1;
+      flex-basis: max-content;
+      overflow-y: scroll;
+`
+    return {Card, Image, Title, Description, Author, Content}
 }
 
 export default PostComponent
